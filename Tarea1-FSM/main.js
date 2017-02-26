00e510359709419c240cf0a3d43900b9766cb960
Felipe Mejías <fmejias19@gmail.com>
2017-02-26 13:42:01 -0600

    V 1.00 - Implementación de estados y de eventos

--- Tarea1-FSM/main.js
+++ Tarea1-FSM/main.js
@@ -17,8 +17,10 @@ var state_machine = new FSM();
 /**
 *Second, we have to create the states of the FSM
 */
-var state_1 = new State();
-var state_2 = new State();
+var state_1 = new State(); //This state is going to be: Descansando
+var state_2 = new State(); //This state is going to be: Enojado
+var state_3 = new State(); //This state is going to be: Molesto 
+var state_4 = new State(); //This state is going to be Furioso
 
 /**
 *Third, we have to create the owner the FSM
@@ -28,45 +30,95 @@ owner_1.assignOwnerInformation("FSM1", 1);
 
 
 /*
- * Now, we have to create the Event Objects of the first state (Apagado)
+ * Now, we have to create the Event Objects of the first state (Descansando)
  *
  */
  var event_1 = new Event();
- event_1.assignEventInformation("apagar", "Apagado");
+ event_1.assignEventInformation("Herido", "Enojado");
 
  var event_2 = new Event();
- event_2.assignEventInformation("encender", "Encendido"); 
+ event_2.assignEventInformation("Elemento en su area", "Molesto"); 
+
+ var event_3 = new Event();
+ event_3.assignEventInformation("Elemento fuera de su area", "Descansando"); 
+
+ var event_4 = new Event();
+ event_4.assignEventInformation("Sanando", "Descansando"); 
 
 /*
- * Now, we have to create the Event Objects of the second state (Encendido)
+ * Now, we have to create the Event Objects of the second state (Enojado)
  *
  */
 
- var event_3 = new Event();
- event_3.assignEventInformation("apagar", "Apagado");
+ var event_5 = new Event();
+ event_5.assignEventInformation("Sanando", "Molesto");
 
- var event_4 = new Event();
- event_4.assignEventInformation("encender", "Encendido");
+ var event_6= new Event();
+ event_6.assignEventInformation("Herido", "Furioso");
+
+ var event_7 = new Event();
+ event_5.assignEventInformation("Elemento en su area", "Enojado");
+
+ var event_8= new Event();
+ event_6.assignEventInformation("Elemento fuera de su area", "Enojado");
+
+ /*
+ * Now, we have to create the Event Objects of the third state (Molesto)
+ *
+ */
+
+ var event_9 = new Event();
+ event_9.assignEventInformation("Herido", "Enojado");
+
+ var event_10= new Event();
+ event_10.assignEventInformation("Sanando", "Descansando");
+
+ var event_11 = new Event();
+ event_11.assignEventInformation("Elemento en su area", "Molesto");
+
+ var event_12= new Event();
+ event_12.assignEventInformation("Elemento fuera de su area", "Descansando");
+
+ /*
+ * Now, we have to create the Event Objects of the fourth state (Furioso)
+ *
+ */
+
+ var event_13 = new Event();
+ event_13.assignEventInformation("Sanando", "Enojado");
+
+ var event_14= new Event();
+ event_14.assignEventInformation("Herido", "Furioso");
+
+ var event_15 = new Event();
+ event_15.assignEventInformation("Elemento en su area", "Furioso");
+
+ var event_16 = new Event();
+ event_16.assignEventInformation("Elemento fuera de su area", "Furioso");
 
  /*
   * Now we have to add the Event Objects to an array, so we can add it to the list of events of each state
-  * State 1 = Apagado , State2 = Encendido
+  * State 1 = Descansando , State2 = Enojado, State3 = Molesto, State4 = Furioso
   */
 
-  var listOfEventsState1 = [event_1, event_2];
-  var listOfEventsState2 = [event_3, event_4];
+  var listOfEventsState1 = [event_1, event_2, event_3, event_4];
+  var listOfEventsState2 = [event_5, event_6, event_7, event_8];
+  var listOfEventsState3 = [event_9, event_10, event_11, event_12];
+  var listOfEventsState4 = [event_13, event_14, event_15, event_16];
 
 /*
  * Now, we have to add the information of the states
  *
  */
- state_1.assignStateInformation("Apagado", "activo", listOfEventsState1);
- state_2.assignStateInformation("Encendido", "inactivo", listOfEventsState2);
+ state_1.assignStateInformation("Descansando", "activo", listOfEventsState1);
+ state_2.assignStateInformation("Enojado", "inactivo", listOfEventsState2);
+ state_3.assignStateInformation("Molesto", "inactivo", listOfEventsState3);
+ state_4.assignStateInformation("Furioso", "inactivo", listOfEventsState4);
  
  /*
   * Then. we have to create the list of sttes
   */
- var listOfStates = [state_1, state_2];
+ var listOfStates = [state_1, state_2, state_3, state_4];
 
  /*
   * Now, we have to update the state machine attributes
@@ -83,7 +135,7 @@ eventEmitter.register(state_machine);
  */
 
 //We define a new variable to change messages
-var message = "encender";
+var message = "Herido";
 
 //This function is changing the message
 function changeMessage(){
