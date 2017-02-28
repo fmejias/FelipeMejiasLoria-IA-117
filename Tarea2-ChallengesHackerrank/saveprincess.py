c410e674245dea0b3ab6ae28e2891e57e62b59c9
Felipe Mejías <fmejias19@gmail.com>
2017-02-27 23:45:57 -0600

    V 2.00 - Versión mejorada del ejercicio: saveprincess

--- Tarea2-ChallengesHackerrank/saveprincess.py
+++ Tarea2-ChallengesHackerrank/saveprincess.py
@@ -8,22 +8,23 @@ def displayPathtoPrincess(n,grid):
         grid.append(input().strip())
     
     #Here we print all the moves of the Bot
-    print("DOWN")
-    print("LEFT")
-    if(grid[2][0] == "p"):
-        return "La encontre"
-    print("UP")
-    print("UP")
-    if(grid[0][0] == "p"):
-        return "La encontre"
-    print("RIGHT")
-    print("RIGHT")
-    if(grid[0][2] == "p"):
-        return "La encontre"
-    print("DOWN")
-    print("DOWN")
-    if(grid[2][2] == "p"):
-        return "La encontre"
+    if(grid[2][0] == "p" or grid[2][2] == "p"):
+        print("DOWN")
+        if(grid[2][0] == "p"):
+            print("LEFT")
+            return "La encontre"
+        else:
+            print("RIGHT")
+            return "La encontre"
+    elif(grid[0][0] == "p" or grid[0][2] == "p"):
+        print("UP")
+        if(grid[0][0] == "p"):
+            print("LEFT")
+            return "La encontre"
+        else:
+            print("RIGHT")
+            return "La encontre"
+    
     
 #Here we call the function to display the path to the princess    
 displayPathtoPrincess(3, [])
