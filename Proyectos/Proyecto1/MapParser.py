1e8dcd27246308f2166410cf23d2f80d5364150e
Felipe Mejías <fmejias19@gmail.com>
2017-04-09 22:01:54 -0600

    V 3.00 Display Images Of The City - First Implementation

--- Proyectos/Proyecto1/MapParser.py
+++ Proyectos/Proyecto1/MapParser.py
@@ -22,6 +22,8 @@ class MapParser:
         self.maxNumberOfCharacters = self.getMaxNumberOfCharacters()
         self.intermediateFile = "mapParse.txt"
         self.newFile = "mapFinalParse.txt"
+        self.city = []
+        self.cityForTkinter = []
 
 
     #This method is in charge of print the Map for the Taxi Simulation
@@ -70,6 +72,9 @@ class MapParser:
         newFile.close()
         mapFile.close()
 
+        #Call inmediatly the reviewNewMap
+        self.reviewNewMap()
+
     #This method is use to review the new map, and put the * that are ausent.
     def reviewNewMap(self):
         newString = "*"
@@ -93,16 +98,33 @@ class MapParser:
             if((line[firstNewStringPosition-1] == "|" or line[firstNewStringPosition-1] == "-") and line[firstNewStringPosition] == " " and j!= -1):
                 for i in range(firstNewStringPosition,firstNewStringPosition+j):
                     newLine = newLine + newString
+
+                #Add the row to the city matrix
+                lineToAppend = line[0:firstNewStringPosition] + newLine + line[firstNewStringPosition+j:self.maxNumberOfCharacters]                    
+                self.city.append(list(lineToAppend))
+                self.cityForTkinter.append(list(lineToAppend))
                 newLine = line[0:firstNewStringPosition] + newLine + line[firstNewStringPosition+j:self.maxNumberOfCharacters] + "\n"
                 finalMapFile.write(newLine)
+
             elif((line[firstNewStringPosition-1] == "|" or line[firstNewStringPosition-1] == "-") and line[firstNewStringPosition] == " " and k!= -1):
                 for x in range(firstNewStringPosition,firstNewStringPosition+k):
                     newLine = newLine + newString
+
+                #Add the row to the city matrix
+                lineToAppend = line[0:firstNewStringPosition] + newLine + line[firstNewStringPosition+k:self.maxNumberOfCharacters] 
+                self.city.append(list(lineToAppend))
+                self.cityForTkinter.append(list(lineToAppend))
                 newLine = line[0:firstNewStringPosition] + newLine + line[firstNewStringPosition+k:self.maxNumberOfCharacters] + "\n"
                 finalMapFile.write(newLine)
+ 
             else:
                 newLine = line
                 finalMapFile.write(newLine)
+
+                #Add the row to the city matrix
+                lineToAppend = line.replace("\n","")
+                self.city.append(list(lineToAppend))
+                self.cityForTkinter.append(list(lineToAppend))
 
         #Close all of the files
         newFile.close()
@@ -111,18 +133,32 @@ class MapParser:
     #This method is in charge of print the new map, after parse it.
     def printNewMap(self):
         newFile = open(self.newFile,'r') #Open the file with the map specification in read mode
+
         #Print all the content of the file
         for line in newFile:
             print(line, end='')
 
         #Close the map file
         newFile.close()
-        
 
+    #This method is use to get the matrix of the city
+    def getCity(self):
+        for i in range(0, len(self.city)):
+            for j in range(0,len(self.city[0])):
+                if( (i - 1 >= 0) and (i + 1 < len(self.city)) and (j-1 >= 0) and (j+1 < len(self.city[0]))):                    
+                    if((self.city[i-1][j] == "-") and (self.city[i+1][j] == "-") and (self.city[i][j-1] == "|") and (self.city[i][j+1] == "|")):
+                        self.cityForTkinter[i][j] = ("yes", self.city[i][j])
+                    else:
+                        self.cityForTkinter[i][j] = ("no", self.city[i][j])
+                else:
+                    self.cityForTkinter[i][j] = ("no", self.city[i][j])
+                    
+        return self.cityForTkinter
+        
 
 #This method creates an instance of the class MapParser
 def createMapParser():
-    parser = MapParser("mapa1.txt")
-    parser.generateNewMap()
-    parser.reviewNewMap()
-    parser.printNewMap()
+    parser = MapParser("mapa1.txt") #Creates the MapParser Object
+    parser.generateNewMap() #This instruction call the method of the MapParser Object that generates the new map
+    x = parser.getCity()
+    return parser.getCity() #Returns the city matrix
--- Proyectos/Proyecto1/TaxiSimulationGraphicalInterface.py
+++ Proyectos/Proyecto1/TaxiSimulationGraphicalInterface.py
@@ -6,6 +6,14 @@
 
 #Import of the standard Python interface to the Tk GUI toolkit
 from tkinter import*
+from PIL import ImageTk, Image
+
+
+#Import from the MapParser module the city matrix
+import MapParser
+
+#Import to generate random numbers
+from random import randint
 
 ###########################################################################################################
 # TaxiSimulationWindow Class:
@@ -13,35 +21,63 @@ from tkinter import*
 # Methods:
 # .
 ##########################################################################################################
-
 class TaxiSimulationWindow:
     def __init__(self, master):
+        #Establish the width and height of the First Window
+        self.width = 1100
+        self.height = 650
+        
+        #Initialize the city matrix, get the number of rows of the city and the number of columns of the city
+        self.city = MapParser.createMapParser()
+        self.rows = len(self.city)
+        self.columns = len(self.city[0])
 
-        #Initialize the instruction variable
-        self.instruction = ""
+        #Initialize the list of Labels that are going to compose the Window
+        self.listOfLabels = []
 
-        #Initialize the variables to position the entries
-        self.x = 0
-        self.y = 20
-
-        #Initialize the variable that contains the actual instruction entry
-        self.actualEntry = ""
-
+        #Set all of the images
+        self.taxiImage = PhotoImage(file="ProjectImages/taxi1.png")
+        self.wallImage = PhotoImage(file="prueba.png")
+        self.img = Image.open("barrera4.png")
+        
+        
         #Set the master as the root
         self.master = master
 
-        #Here, we create a frame
-        self.frame = Frame(self.master, width=1000, height=600, background="White")
-        self.frame.pack()
+        #Here, we call the function in charge of build the city
+        self.buildCity()
 
+    #This method is in charge of build the city
+    def buildCity(self):
+        widthOfEachFrame = self.width // self.columns
+        heightOfEachFrame = self.height // self.rows
+        colors = ["Red", "Green", "Yellow", "Blue"]
+
+        #Go over all the city matrix
+        for i in range (0, self.rows):
+            for j in range (0,self.columns):
+                randomIndex = randint(0,3)
+                frame=Frame(self.master, width=widthOfEachFrame, height=heightOfEachFrame, background=colors[randomIndex])
+                frame.grid(row=i, column=j)
+
+                #Resize the image with the size of the square
+                displayImage = self.img.resize((widthOfEachFrame, heightOfEachFrame), Image.ANTIALIAS)
+                displayImage = ImageTk.PhotoImage(displayImage)
+
+                #Create the Label and add it to the List of Labels
+                label = Label(frame, image = displayImage)
+                label.image = displayImage
+                label.place(x=0,y=0)
+                self.listOfLabels.append(label) #Add the Label to the list
+        
 
 
 #This function display the taxi simulation
 def displayTaxiSimulation():
     master = Tk()#Create the principle window
-    master.wm_title("Taxi Simulation Window") #Add a title to the window
+    master.wm_title("Taxi Simulation") #Add a title to the window
     taxiSimulationWindow = TaxiSimulationWindow(master) #Add the taxi simulation frame to the principle window
-    master.geometry("1000x600") #Set the size of the root
-    master.geometry("+10+50") #Set the position of the root on the screen
+    master.geometry("1100x650") #Set the size of the root
+    master.geometry("+0+10") #Set the position of the root on the screen
     master.resizable(width=NO,height=NO) #Set the window as no resizable
     master.mainloop() #Starts the mainloop of the console window
